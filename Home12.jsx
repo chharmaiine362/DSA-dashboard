f49ac3de0048a8b5dc213e25441272a3d1845e48
chharmaiine362 <158123616+chharmaiine362@users.noreply.github.com>
2024-04-24 00:12:50 +0800

    latest

--- Home12.jsx
+++ Home12.jsx
@@ -62,34 +62,34 @@ function Home12() {
             <h3>DASHBOARD</h3>
         </div>
 
-        <div className='main-cards'>
+        <div className='main-cards text-white'>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>PRODUCTS</h3>
+                    <h3 className='text-color'>Tickets Bought</h3>
                     <BsFillArchiveFill className='card_icon'/>
                 </div>
-                <h1>300</h1>
+                <h1 className='text-white text-color'>300</h1>
             </div>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>CATEGORIES</h3>
+                    <h3 className='text-white text-color'>Revenue</h3>
                     <BsFillGrid3X3GapFill className='card_icon'/>
                 </div>
-                <h1>12</h1>
+                <h1 className='text-white text-color'>12</h1>
             </div>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>CUSTOMERS</h3>
+                    <h3 className='text-white text-color'>Customers</h3>
                     <BsPeopleFill className='card_icon'/>
                 </div>
-                <h1>33</h1>
+                <h1 className='text-white text-color'>33</h1>
             </div>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>ALERTS</h3>
+                    <h3 className='text-white text-color' >Temperature</h3>
                     <BsFillBellFill className='card_icon'/>
                 </div>
-                <h1>42</h1>
+                <h1 className='text-white text-color'>42</h1>
             </div>
         </div>
 
