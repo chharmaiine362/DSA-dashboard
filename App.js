e887bdead33d9dd8c04c40959ddf384c6eff555d
Ansel Goh <156350273+anselgoh@users.noreply.github.com>
2024-04-24 19:45:14 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './dashboard/Header'
-import Sidebar from './dashboard/Sidebar'
-import Home12 from './dashboard/Home12'
+import { useState } from 'react';
+import './App.css';
+import Header from './dashboard/Header';
+import Sidebar from './dashboard/Sidebar';
+import Home12 from './dashboard/Home12';
+import Customers from './dashboard/Customers';
+ // Make sure the path is correct
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
 
   const OpenSidebar = () => {
-    setOpenSidebarToggle(!openSidebarToggle)
-  }
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
 
   return (
-    <div className='grid-container'>
-      <Header OpenSidebar={OpenSidebar}/>
-      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
-      <Home12 />
-    </div>
-  )
+    <Router>
+      <div className='grid-container'>
+        <Header OpenSidebar={OpenSidebar}/>
+        <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+        <Routes>
+          <Route path="/" element={<Home12 />} />
+          <Route path="/customers" element={<Customers />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
-
+export default App;
