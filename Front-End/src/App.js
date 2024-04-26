be043ffe2e424ffcb7d1d1515bf3495a696afd6f
chharmaiine362 <158123616+chharmaiine362@users.noreply.github.com>
2024-04-26 20:44:16 +0800

    Update App.js

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -3,18 +3,15 @@ import './App.css';
 import Header from './dashboard/Header';
 import Sidebar from './dashboard/Sidebar';
 import Home12 from './dashboard/Home12';
-import Customers from './dashboard/Customers';
+import Customers from './dashboard/Customers/Customers';
 import FAQ from './dashboard/FAQ';
+ // Make sure the path is correct
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Main application component that sets up the routing and layout structure.
 function App() {
-  
-  // State to manage the sidebar's open/close
   const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
-  
-  // Function to toggle the sidebar's visibility.
+
   const OpenSidebar = () => {
     setOpenSidebarToggle(!openSidebarToggle);
   };
@@ -24,14 +21,11 @@ function App() {
       <div className='grid-container'>
         <Header OpenSidebar={OpenSidebar}/>
         <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
-    
-        {/ Routing setup using React Router /}
         <Routes>
-    
-          <Route path="/" element={<Home12 />} />   // Route to the home page/dashboard 
-          <Route path="/customers" element={<Customers />} />    // Route to the customers page
-          <Route path="/FAQ" element={<FAQ />} />    // Route to the FAQ page
-
+          <Route path="/" element={<Home12 />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/FAQ" element={<FAQ />} />
+          {/* Add more routes as needed */}
         </Routes>
       </div>
     </Router>
